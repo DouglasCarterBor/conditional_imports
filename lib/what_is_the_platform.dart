17761a18a52f6a71f99de0d384d9f20f21e155bb
Douglas Carteri Bordignon <douglasbordignon8@gmail.com>
2023-06-27 21:35:33 -0300

    🎨

--- lib/what_is_the_platform.dart
+++ lib/what_is_the_platform.dart
@@ -1,5 +1,5 @@
 export 'did_not_find.dart'
-  if (dart.library.io) 'mobile.dart'
+  if (dart.library.mobile) 'mobile.dart'
   if (dart.library.html) 'web.dart'
   if (dart.library.macos) 'macos.dart'
   if (dart.library.windows) 'windows.dart'
